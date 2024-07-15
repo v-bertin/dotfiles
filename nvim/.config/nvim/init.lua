4b45d4f424862a7cc7f3f5d62f0f5709fa36c53d
Victor Bertin <victor.bertin@b-on.com>
2024-07-15 11:18:29 +0200

    nvim: add base editor settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,15 @@
 require("config.lazy")
 require("config.telescope")
 require("config.keymaps")
+
+-- Neovim related configs
+
+-- Sync OS and Neovim clipboards
+vim.o.clipboard = 'unnamedplus'
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Print line numbers in the status column
+vim.wo.number = true
+vim.wo.relativenumber = true
