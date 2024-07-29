b09b7fef1cf2755a66c1c55a506ad6de3a0e2431
Victor Bertin <victor.bertin@b-on.com>
2024-07-29 21:50:31 +0200

    nvim: add keymaps to view diagnostics

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,8 @@ keymap("n", "<leader><leader>", "<cmd> Telescope buffers<cr>", opts)
 keymap("n", "<leader>sf", "<cmd> Telescope find_files<cr>", opts)
 keymap("n", "<leader>sg", "<cmd> Telescope live_grep<cr>", opts)
 keymap("n", "<leader>f", "<cmd> Telescope file_browser path=%:p:h select_buffer=true<cr>", opts)
+
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
