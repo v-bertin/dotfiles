3b49ddad3c8454f56560b811afd8a6459f1ddadd
Victor Bertin <victor.bertin@b-on.com>
2024-09-09 10:44:39 +0200

    nvim: notes: fix markdown-preview broken install
    See https://github.com/iamcco/markdown-preview.nvim/pull/691

--- nvim/.config/nvim/lua/plugins/notes.lua
+++ nvim/.config/nvim/lua/plugins/notes.lua
@@ -8,6 +8,9 @@ return {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
+        build = function()
+            require("lazy").load { plugins = { "markdown-preview.nvim" } }
+            vim.fn["mkdp#util#install"]()
+        end,
     },
 }
