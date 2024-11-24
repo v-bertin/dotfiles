f072d4629178e49e00fd603bfc27d788a4d21677
Victor Bertin <victor.bertin@b-on.com>
2024-11-24 17:29:03 +0100

    nvim: add new cool plugins
    - compile-mode.nvim : run commands and get their output in a neovim
      buffer (basically, emacs compilation mode in neovim)
    - vim-abolish : easily switch from one spelling case to another

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -19,4 +19,27 @@ return {
         'saecki/crates.nvim',
         tag = 'stable',
     },
+    -- simili emacs compilation mode
+    {
+        "ej-shafran/compile-mode.nvim",
+        branch = "latest",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            -- if you want to enable coloring of ANSI escape codes in
+            -- compilation output, add:
+            -- { "m00qek/baleia.nvim", tag = "v1.3.0" },
+        },
+        config = function()
+            vim.g.compile_mode = {
+                -- to add ANSI escape code support, add:
+                -- baleia_setup = true,
+            }
+        end
+    },
+    -- coerce from a case to another
+    -- crs - coerce to snake_case
+    -- crm - coerce to MixedCase
+    -- crc - coerce to camelCase
+    -- cru - coerce to UPPER_CASE
+    { "tpope/vim-abolish" },
 }
