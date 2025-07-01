734fd94bec7c1e6072594556a82c70ab4007d06e
Victor Bertin <victor.bertin@b-on.com>
2025-07-01 14:48:59 +0200

    nvim: highlight vspec file as yaml

--- nvim/.config/nvim/lua/config/treesitter.lua
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -1,31 +1,39 @@
 require("nvim-treesitter.configs").setup({
-	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-	ensure_installed = {
-		"bash",
-		"c",
-		"cmake",
-		"devicetree",
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "devicetree",
         "kconfig",
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"python",
-		"rust",
-	},
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "rust",
+        "yaml",
+    },
 
     -- List of parsers to ignore installing (or "all")
     ignore_install = {},
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = false,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = false,
 
-	highlight = {
-		enable = true,
-	},
+    highlight = {
+        enable = true,
+    },
 
     modules = {},
+})
+
+vim.filetype.add({
+    extension = {
+        vspec = 'yaml',
+    },
 })
